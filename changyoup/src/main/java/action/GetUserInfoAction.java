44dc8f986403ee8cfbfe61d97bcd3d6162733de6
ReZG\ReZG <renzhi_paul@sjtu.edu.cn>
2017-06-30 12:38:04 +0800

    UserCenter 查看修改个人信息

---
+++ changyoup/src/main/java/action/GetUserInfoAction.java
@@ -0,0 +1,89 @@
+package action;
+
+import java.sql.Date;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import model.UserInfo;
+import service.UserInfoService;
+
+public class GetUserInfoAction extends ActionSupport{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String nickname;
+	private String email;
+	private String area;
+	private int sex;
+	private Date birthday;
+	
+	private UserInfoService userinfoService;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname=nickname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getArea(){
+		return area;
+	}
+	
+	public void setArea(String area){
+		this.area=area;
+	}
+	public int getSex(){
+		return sex;
+	}
+	
+	public void setSex(int sex){
+		this.sex=sex;
+	}
+	
+	public Date getBirthday(){
+		return birthday;
+	}
+	
+	public void setBirthday(Date birthday){
+		this.birthday=birthday;
+	}
+	
+	public void setUserinfoService(UserInfoService userinfoService){
+		this.userinfoService=userinfoService;
+	}
+	
+	@Override
+	public String execute() throws Exception {
+
+		UserInfo userinfo = userinfoService.getUserInfoById(id);
+		userinfo.setNickname(nickname);
+		userinfo.setEmail(email);
+		userinfo.setArea(area);
+		userinfo.setSex(sex);
+		userinfo.setBirthday(birthday);
+		userinfoService.updateUserInfo(userinfo);
+
+		return SUCCESS;
+	}	
+}
--- changyoup/src/main/webapp/changyoup/jsp/Usercenter.jsp
+++ changyoup/src/main/webapp/changyoup/jsp/Usercenter.jsp
@@ -18,13 +18,13 @@
 <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
 
     <div class="navbar-header">
-        <a class="navbar-brand" href="#">菜鸟教程</a>
+        <a class="navbar-brand" href="#">个人中心</a>
     </div>
     
     <div class="navbar-default sidebar" role="navigation">
     <div class="sidebar-nav navbar-collapse">
         <ul class="nav" id="side-menu">
-            <li class="active"><a href="#">iOS</a></li>
+            <li class="active"><a href="Account">个人资料修改</a></li>
             <li><a href="#">SVN</a></li>
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -50,7 +50,7 @@
 <div id="page-wrapper">
 	<div class="row">
 		<div class="col-lg-12">
-			<h1 class="page-header">Users</h1>
+			<h1 class="page-header">UserInfo</h1>
 		</div>
 	</div>
 </div>
