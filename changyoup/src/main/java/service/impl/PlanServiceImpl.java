49d94a9e350f869d8ee78692c935ab0d48de686a
RX-78-2\jinxueyi <aosilujia@gmail.com>
2017-07-14 09:29:44 +0800

    planserviceimpl

--- changyoup/src/main/java/service/impl/PlanServiceImpl.java
+++ changyoup/src/main/java/service/impl/PlanServiceImpl.java
@@ -1,20 +1,37 @@
 package service.impl;
 
+import java.util.List;
+
 import dao.PlanDao;
+import dao.PlaninfoDao;
+import model.Planinfo;
 import service.PlanService;
 
 
 public class PlanServiceImpl implements PlanService{
 
 	private PlanDao planDao;
+	private PlaninfoDao planinfoDao;
 	
 	public void setPlanDao(PlanDao planDao) {
 		this.planDao = planDao;
 	}
+	
+	public void setPlaninfoDao(PlaninfoDao planinfoDao) {
+		this.planinfoDao = planinfoDao;
+	}
+	
 	@Override
-	public Integer addPlan(int planid, String plan) {
-		// TODO Auto-generated method stub
-		return null;
+	public Integer addPlan(Planinfo planinfo, String plan) {
+		planinfoDao.save(planinfo);
+		int planid=planinfo.getPlanid();
+		try {
+			planDao.save(planid, plan);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return planid;
 	}
 
 	@Override
@@ -23,14 +40,11 @@ public class PlanServiceImpl implements PlanService{
 			planDao.save(planid, plan);
 		} catch (Exception e) {
 			
-		}
-		
+		}	
 	}
 
 	@Override
 	public void deletePlan(int planid) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
@@ -43,6 +57,26 @@ public class PlanServiceImpl implements PlanService{
 		catch(Exception e){
 			return null;
 		}
+	}
+
+	@Override
+	public void updatePlaninfo(Planinfo planinfo) {
+		planinfoDao.update(planinfo);
+	}
+
+	@Override
+	public List<Planinfo> getPlanbyPublisher(int userid) {
+		return planinfoDao.getPlaninfoByPublisher(userid);
+	}
+
+	@Override
+	public List<Planinfo> getUnpassedPlan() {
+		return planinfoDao.getUnpassedPlan();
+	}
+
+	@Override
+	public List<Planinfo> getAllpassedPlan() {
+		return planinfoDao.getAllpassedPlan();
 	}
 
 }
